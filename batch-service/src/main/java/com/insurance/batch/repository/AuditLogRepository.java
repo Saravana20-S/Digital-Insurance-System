8a352b4a9e20f0ec45e8c4b87d54017da3a4d235
Saravana20-S <iamsaravanan1347@gmail.com>
2026-09-16 16:30:50 +0530

    feat: Audit log repo logic added

---
+++ batch-service/src/main/java/com/insurance/batch/repository/AuditLogRepository.java
@@ -0,0 +1,8 @@
+package com.insurance.batch.repository;
+
+import com.insurance.batch.entity.AuditLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuditLogRepository
+        extends JpaRepository<AuditLog, Long> {
+}
